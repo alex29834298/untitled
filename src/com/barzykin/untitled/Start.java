f34df51407d57e1deb802ef7af691cb7352175af
alex <alex@email.com>
2021-09-16 19:58:54 +0300

    World is added

--- src/com/barzykin/untitled/Start.java
+++ src/com/barzykin/untitled/Start.java
@@ -2,6 +2,6 @@ package com.barzykin.untitled;
 
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello, World");
     }
 }
